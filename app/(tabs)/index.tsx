6564501c82fc024c5c42553c27b7cf03dfadc09e
Amir Iqbal <48883764+amiriqbalmcs@users.noreply.github.com>
2025-08-28 16:51:23 +0500

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -185,7 +185,7 @@ const styles = StyleSheet.create({
   },
   statsGrid: {
     flexDirection: 'row',
-    paddingHorizontal: 12,
+    paddingHorizontal: 0,
   },
   fullWidthCard: {
     paddingHorizontal: 12,
