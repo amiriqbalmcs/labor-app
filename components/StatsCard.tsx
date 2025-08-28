66c7bb04979bf73ac2fb98caca9dd8d01b17bf24
Amir Iqbal <48883764+amiriqbalmcs@users.noreply.github.com>
2025-08-28 16:57:04 +0500

    Updated StatsCard.tsx

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -24,7 +24,7 @@ export function StatsCard({ title, value, icon, color = '#2563eb', theme = 'ligh
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
+    flex: 1,
     margin: 4,
   },
   darkContainer: {
